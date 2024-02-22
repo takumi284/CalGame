30bbfc2dc23c8e3d3e1ffe22b7d8e730c4230ba3
kosuke <221205102@ccmailg.meijo-u.ac.jp>
2024-02-22 18:17:53 +0900

    関数訂正
    余分な書き方を直した

--- src/background.jsx
+++ src/background.jsx
@@ -1,6 +1,5 @@
 operator = ['','+', '-', '*', '/', '(', ')'];
 
-
 function CorrectWrong(list){
     var formula = '';
     //listの1番目/15番目の文字
@@ -20,87 +19,79 @@ function CorrectWrong(list){
     //listの3番目/15番目の文字
     if (list[2] === 0){
         //空白の場合
-    }else if(list[2] === 5){
-        //( の場合
-        formula += operator[5];
-    }else{
-        //それ以外の演算子の場合
-        //計算式として成り立たない
+    }else if(list[2] === 5 || list[2] === 6){
         return false;
+    }else{
+        formula += operator[list[2]];
     }
-
-    //listの4番目(数字部分）/15番目の文字
+    
+    //list[3]
     if (list[3] === 0){
         //空白の場合
-    }else if(list[3] === 5 || list[3] === 6){
-        return false;
-    }else{
-        formula += operator[list[3]];
-    }
-
-    //listの5番目(数字部分）/15番目の文字
-    if (list[4] === 0){
-        //空白の場合
-    }else if(list[4] === 5){
+    }else if(list[3] === 5){
         //( の場合
-        formula = formula + operator[5];
+        formula = formula + operator[3];
     }else{
         //それ以外の演算子の場合
         //計算式として成り立たない
         return false;
     }
 
-    //listの6番目(数字部分）/15番目の文字
-    formula += list[5];
+    //list[4]
+    formula += list[4];
 
-    //listの7番目/15番目の文字
+    //list[5]
+    if (list[5] === 0){
+        //空白の場合
+    }else if(list[5] === 5 || list[5] === 6){
+        formula += operator[list[5]];
+    }else{
+        return false;
+    }
+
+    //list[6]
     if (list[6] === 0){
         //空白の場合
     }else if(list[6] === 5 || list[6] === 6){
-        formula += operator[list[6]];
-    }else{
         return false;
+    }else{
+        formula += operator[list[6]];
     }
 
-    //listの8番目/15番目の文字
+    //list[7]
     if (list[7] === 0){
         //空白の場合
     }else if(list[7] === 5 || list[7] === 6){
-        return false;
-    }else{
         formula += operator[list[7]];
-    }
-
-    //listの9番目/15番目の文字
-    if (list[8] === 0){
-        //空白の場合
-    }else if(list[8] === 5 || list[8] === 6){
-        formula += operator[list[8]];
     }else{
         return false;
     }
 
-    //listの10番目(数字部分）/15番目の文字
-    formula += list[9];
+    //list[8]
+    formula += list[8];
 
-    //listの11番目/15番目の文字
+    //list[9]
+    if (list[9] === 0){
+        //空白の場合
+    }else if(list[9] === 6){
+        formula += operator[list[9]];
+    }else{
+        return false;
+    }
+
+    //list[10]
     if (list[10] === 0){
         //空白の場合
-    }else if(list[10] === 6){
+    }else if(list[10] === 5 || list[10] === 6){
+        return false;
+    }else{
         formula += operator[list[10]];
-    }else{
-        return false;
     }
+    
+    //list[11]
+    formula += list[11];
 
-    //listの12番目/15番目の文字
-    if (list[11] === 0){
-        //空白の場合
-    }else if(list[11] === 5 || list[11] === 6){
-        return false;
-    }else{
-        formula += operator[list[11]];
-    }
-    //listの13番目/15番目の文字
+    //list[12]
     if (list[12] === 0){
         //空白の場合
     }else if(list[12] === 6){
@@ -108,17 +99,7 @@ function CorrectWrong(list){
     }else{
         return false;
     }
-    //listの14番目/15番目の文字
-    formula += list[13];
 
-    //listの15番目/15番目の文字
-    if (list[14] === 0){
-        //空白の場合
-    }else if(list[14] === 6){
-        formula += operator[list[14]];
-    }else{
-        return false;
-    }
     return eval(formula);
 }
 function check(eq){
@@ -131,9 +112,8 @@ function check(eq){
     }
 }
 
-//check(CorrectWrong([5,4,0,3,0,2,6,1,0,3,0,2,0,1,0]))
+//check(CorrectWrong([5,4,3,0,2,6,1,0,3,0,2,1,0]))
 //を実行すると、1が返ってくる
 //(4 * 2)+ 3 - 1  = 10
-//540302610302010
 
 
